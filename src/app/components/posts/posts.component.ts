91bd5e9eac2a002256878bb0050a01de356af457
Uriel Shrem <urielshrem@gmail.com>
2021-10-17 18:06:20 +0300

    amount of posts per page

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -10,8 +10,14 @@ import { PostService } from 'src/app/services/post.service';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  posts: post;
-
+  posts: post[]=[];
+  index=0;
+  postsOnPage = 5;
+  postToPrint:post[]=[];
+  next =false;
+  backe=false;
+  lengthPosts:number;
+  
   constructor(private servicPost:PostService, private router: Router ) { }
 
   ngOnInit(): void {
@@ -19,18 +25,41 @@ export class PostsComponent implements OnInit {
   }
   postGet() : void{
     
-    this.servicPost.getAllPosts().subscribe((data:post) =>{
-     
-      this.posts= data;
-      
-      
-
-
-    
+    this.servicPost.getAllPosts().subscribe((data:post[]) =>{
+      data.forEach(e => {
+        this.posts.push(e);
+    });
    })
-   
-   
-   
  }
 
+  printPost(){
+    for (let i = 0; i < this.postsOnPage; i++) {
+      if (this.posts[this.index]) {    
+      this.postToPrint[i]=this.posts[this.index];
+      this.index++;
+      if (this.index+this.postsOnPage<= this.posts.length) {
+        this.next=true;
+      }else{
+        this.next=false;
+      }
+      if (this.index>this.postsOnPage) {
+        
+        this.backe=true
+      }else{
+        this.backe=false;
+      }
+      
+      }
+      
+    }
+  }
+  nextPage(){ 
+      this.printPost()
+      
+    }
+  backePage(){ 
+  
+      this.index= this.index-10;
+      this.printPost()
+    }
 }
